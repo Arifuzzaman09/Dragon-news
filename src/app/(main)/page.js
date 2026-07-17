8b77e633f7a7fc435d1fd0e366f0dc63edca9534
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-07-17 22:44:33 +0600

    added left side bar

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,37 @@
-import Image from "next/image";
 
-export default function Home() {
+
+const getCategory = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
+  const data = await res.json();
+
+  return data.data
+
+}
+
+
+export default async function Home() {
+
+  const categorys = await getCategory();
+  console.log(categorys.news_category);
+
   return (
-   <div>
-   
-   </div>
+    <div className="container mx-auto grid grid-cols-12 gap-5 my-7">
+      <div className="  col-span-3">
+        <h2 className="text-lg  font-bold">All Catagory</h2>
+        <ul className="space-y-2 mt-6 ">
+          {
+            categorys.news_category.map(category => {
+              return <li className="text-xl rounded-md p-2 font-bold bg-slate-100 text-center" key={category.category_id}>{category.category_name}</li>
+            })
+          }
+        </ul>
+      </div>
+      <div className="bg-purple-300 text-2xl text-center font-bold col-span-6">
+        All News
+      </div>
+      <div className="bg-red-300 text-2xl text-center font-bold col-span-3">
+        Social Ads
+      </div>
+    </div>
   );
 }
