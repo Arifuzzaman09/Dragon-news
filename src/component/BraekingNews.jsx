8bbec6ca1931a99fa9d09c33aa5dfd0a991d26f1
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-07-17 21:41:42 +0600

    added navbar

--- src/component/BraekingNews.jsx
+++ src/component/BraekingNews.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
+const news =
+    [
+        {
+            "id": 1,
+            "title": "Learn modern React development "
+        },
+        {
+            "id": 2,
+            "title": "Master JavaScript fundamentals through interactive"
+        },
+        {
+            "id": 3,
+            "title": "Build responsive websites using HTML CSS Tailwind and React"
+        },
+        {
+            "id": 4,
+            "title": "Understand backend development with Node Express"
+        },
+        {
+            "id": 5,
+            "title": "Create beautiful user interfaces with reusable"
+        },
+        {
+            "id": 6,
+            "title": "Improve coding skills by solving programming problems"
+        },
+        {
+            "id": 7,
+            "title": "Deploy full stack applications using modern"
+        },
+        {
+            "id": 8,
+            "title": "Explore Git and GitHub workflows for better"
+        },
+        {
+            "id": 9,
+            "title": "Develop professional portfolio projects"
+        },
+        {
+            "id": 10,
+            "title": "Start your web development career with dedication continuous"
+        }
+    ]
+
 const BraekingNews = () => {
     return (
         <div className='flex justify-between gap-4 container mx-auto bg-gray-200 py-4 px-2'>
             <button className='btn bg-red-900 text-white'>Latest News</button>
-            <Marquee>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, reprehenderit ducimus deleniti praesentium blanditiis voluptatibus ad perferendis dolorem.</Marquee>
+            <Marquee pauseOnHover={true}>
+                {news.map(n=>{
+                   return <span key={n.id} className='mx-6'> {n.title} </span>
+                })}
+            </Marquee>
         </div>
     );
 };
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,24 @@
+import Link from 'next/link';
 import React from 'react';
+import userImg from '@/assets/user.png'
+import Image from 'next/image';
+import Navlink from './Navlink';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is Navbar</h1>
+        <div className='container mx-auto flex justify-between items-center py-5'>
+            <div></div>
+            <ul className='flex justify-center gap-3'>
+                <li><Navlink href={'/'}>Home</Navlink></li>
+                <li><Navlink href={'/about'}>About</Navlink></li>
+                <li><Navlink href={'/career'}>Career</Navlink></li>
+            </ul>
+            <div className='flex justify-center gap-2 items-center'>
+                <Image src={userImg} alt={"user-img"} width={50} height={50}></Image>
+                <button className='btn btn-primary'>
+                    <Link href={"/login"}>Login</Link>
+                </button>
+            </div>
         </div>
     );
 };
